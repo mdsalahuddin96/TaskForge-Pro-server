5fdc9a513e979126360371e6fea48434e5112567
mdsalauddin96 <salauddincse96@gmail.com>
2026-06-21 07:54:06 +0600

    create an api for post data to task collection

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-const express=require('express')
-const dotenv=require("dotenv")
-const cors=require("cors");
-const PORT=process.env.PORT||8000
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
 dotenv.config();
-const app=express()
-app.use(cors())
-app.get("/",(req,res)=>{
-    res.send("Hello world")
-})
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log(`Example APP Running on port ${PORT}`)
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_DB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = await client.db("TaskForgeDB");
+    const taskCollection=db.collection("tasks")
+
+    app.post("/api/create/task", async(req,res)=>{
+        const data=req.body;
+        const taskData={
+            ...data,
+            createdAt:new Date()
+        }
+        const result=await taskCollection.insertOne(taskData)
+        res.json(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example APP Running on port ${PORT}`);
+});
